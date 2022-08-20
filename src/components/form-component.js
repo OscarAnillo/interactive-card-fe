6db476cc3f991df1e076a1e1d2194d0fba5f739b
oscar anillo <oscarani14@gmail.com>
2022-08-19 21:32:46 -0500

    add bg image to App

--- src/components/form-component.js
+++ src/components/form-component.js
@@ -1,19 +1,42 @@
+import { useState } from 'react';
+
 export default function FormComponent(){
+    const [userInput, setUserInput] = useState({
+        name: '',
+        number: '',
+        month: '',
+        year: '',
+        cvv: ''
+    })
+
+    const changeHandler = (e) => {
+        const {name, value} = e.target;
+        setUserInput({
+            ...userInput,
+            [name] : value
+        })
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+    }
+
+    const {name, number, month, year, cvv} = userInput;
     return (
         <section className="section-text">
-            <form>
+            <form onSubmit={submitHandler}>
                 <label>Cardholder name</label>
-                <input type="text" placeholder="e.g. Oscar Anillo"/>
+                <input type="text" placeholder="e.g. Oscar Anillo" name="name" value={name} onChange={changeHandler}/>
                 <label>Card number</label>
-                <input type="number" placeholder="e.g. 1234 5678 9123 0000"/>
+                <input type="number" placeholder="e.g. 1234 5678 9123 0000" name="number" value={number} onChange={changeHandler}/>
                     <div>
                         <label>Exp. date (mm/yy)</label>
                         <label className="label-cvv">cvv</label>
                         <div className="form-row">
-                            <input type="number" placeholder="MM" className="mm"/>
-                            <input type="number" placeholder="YY" className="yy"/>
+                            <input type="number" placeholder="MM" className="mm" name="month" value={month} onChange={changeHandler}/>
+                            <input type="number" placeholder="YY" className="yy" name="year" value={year} onChange={changeHandler}/>
                         <div>
-                            <input type="number" placeholder="e.g. 123" className="cvv"/>
+                            <input type="number" placeholder="e.g. 123" className="cvv" name="cvv" value={cvv} onChange={changeHandler}/>
                         </div>
                         </div>
                     </div>
--- src/components/images-component.js
+++ src/components/images-component.js
@@ -4,6 +4,7 @@ export default function ImagesComponent(){
             <div className="img-bg"></div>
             <img src="/images/bg-card-back.png" alt="" className="back" />
             <img src="/images/bg-card-front.png" alt="" className="front" />
+            <img src="/images/card-logo.svg" alt="" className="logo" />
         </section>
     )
 }
